7ecc9cd1f36f0e60ff2a526ef35e30066234ae49
zengfei <453141759@qq.com>
2018-01-18 17:18:35 +0800

    add html-webpack-plugin

--- client/app.js
+++ client/app.js
@@ -1,8 +1,8 @@
 /**
  * Created by Maktub on 2018/1/18
  */
-
+import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 
-ReactDOM.render(App, document.body);
+ReactDOM.render(<App/>, document.body);
