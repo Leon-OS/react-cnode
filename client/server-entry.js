d4c2cea9ece366eecceacce43bf8a0b2594f5283
zengfei <453141759@qq.com>
2018-01-19 12:15:24 +0800

    server-render()

--- client/server-entry.js
+++ client/server-entry.js
@@ -2,7 +2,7 @@
  * Created by Maktub on 2018/1/18
  */
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App.jsx'
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+export default <App/>
+ 
