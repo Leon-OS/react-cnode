d0af0433659e95c443e0445bfe11a57ed6175f3a
zengfei <453141759@qq.com>
2018-01-24 16:51:50 +0800

    axios-> 请求框架 memory-fs -> 测试环境，加载静态资源-内存读取快

--- server/server.js
+++ server/server.js
@@ -6,21 +6,24 @@ const express = require('express');
 const ReactSSR = require('react-dom/server');
 const fs = require('fs');
 const path = require('path');
-const serverEntry = require('../dist/server-entry').default;
 
-const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8');
+const isDev = process.env.NODE_DEV === 'development';
 
 const app = express();
 
-// 区分什么返回静态文件夹，什么返回动态
-app.use('/public', express.static(path.join(__dirname, '../dist')));
-
-app.get('*', function (req, res) {
-	const appString = ReactSSR.renderToString(serverEntry);
-	console.log(appString)
-	res.send(template.replace('<!--app-->', appString))
-});
-
+if (isDev) {
+	const serverEntry = require('../dist/server-entry').default;
+	const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8');
+	// 区分什么返回静态文件夹，什么返回动态
+	app.use('/public', express.static(path.join(__dirname, '../dist')));
+	app.get('*', function (req, res) {
+		const appString = ReactSSR.renderToString(serverEntry);
+		res.send(template.replace('<!--app-->', appString))
+	});
+} else {
+	const devStatic = require('./util/dev-static');
+	devStatic(app)
+}
 
 app.listen(3333, function () {
 	console.log('server is listening on 3333')
---
+++ server/util/dev-static.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Maktub on 2018/1/24
+ */
+const path = require('path');
+const axios = require('axios');
+const webpack = require('webpack');
+const MemoryFs = require('memory-fs');
+const ReactDomServer = require('react-dom/server');
+
+const serverConfig = require('../../build/webpack.config.server');
+
+const getTemplate = () => {
+	return new Promise((resolve, reject) => {
+		axios.get('http://localhost:8888/public/index.html')
+			.then(res => {
+				resolve(res.data)
+			})
+			.catch(reject)
+	})
+};
+
+const Module = module.constructor;
+
+const mfs = new MemoryFs();
+const serverComplier = webpack(serverConfig);
+serverComplier.outputFileSystem = mfs;
+let serverBundle;
+serverComplier.watch({}, (err, status) => {
+	if (err) throw err;
+	status = status.toJson();
+	status.errors.forEach(err => console.error(err));
+	status.warnings.forEach(warn => console.warn(warn));
+
+	const bundlePath = path.join(
+		serverConfig.output.path,
+		serverConfig.output.filename
+	);
+	const bundle = mfs.readFileSync(bundlePath, 'utf-8');
+	const m = new Module();
+	m._compile(bundle, 'server-entry.js'); // 动态编译，指定文件名-> 无法在缓存中读取
+	serverBundle = m.default;
+});
+
+module.exports = function (app) {
+
+	app.get('*', function (req, res) {
+		getTemplate().then(template => {
+			const content = ReactDomServer.renderToString(serverBundle);
+			res.send(template.replace('<!--app-->', content))
+		})
+	})
+};
+ 
