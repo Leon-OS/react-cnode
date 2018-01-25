79b7a2dc0829e30aeb1b9fd95386e1f5c3222391
zengfei <453141759@qq.com>
2018-01-25 15:00:19 +0800

    webpack-抽象

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -1,4 +1,34 @@
 /**
  * Created by Maktub on 2018/1/25
  */
- 
+
+const path = require('path')
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, '../dist'),
+    publicPath: '/public/'
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre', // 编译之前检测
+        test: /.(js|jsx)/,
+        loader: 'eslint-loader',
+        exclude: [
+          path.resolve(__dirname, '../node_modules')
+        ]
+      },
+      {
+        test: /.jsx$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        exclude: path.join(__dirname, '../node_modules')
+      }
+    ]
+  }
+}
+
--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default () => (
-  <div>hello babel</div>
+  <div>hello babel 123</div>
 )
 
 // export default class App extends React.Component {
