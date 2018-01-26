03bf37a82b40f54bd7c2e57fbc7c614475bb004e
zengfei <453141759@qq.com>
2018-01-26 11:00:53 +0800

    fix-lint

--- server/util/dev-static.js
+++ server/util/dev-static.js
@@ -1,57 +1,55 @@
 /**
  * Created by Maktub on 2018/1/24
  */
-const path = require('path');
-const axios = require('axios');
-const proxy = require('http-proxy-middleware');
-const webpack = require('webpack');
-const MemoryFs = require('memory-fs');
-const ReactDomServer = require('react-dom/server');
+const path = require('path')
+const axios = require('axios')
+const proxy = require('http-proxy-middleware')
+const webpack = require('webpack')
+const MemoryFs = require('memory-fs')
+const ReactDomServer = require('react-dom/server')
 
-const serverConfig = require('../../build/webpack.config.server');
+const serverConfig = require('../../build/webpack.config.server')
 
 const getTemplate = () => {
-	return new Promise((resolve, reject) => {
-		axios.get('http://localhost:8888/public/index.html')
-			.then(res => {
-				resolve(res.data)
-			})
-			.catch(reject)
-	})
-};
+  return new Promise((resolve, reject) => {
+    axios.get('http://localhost:8888/public/index.html')
+      .then(res => {
+        resolve(res.data)
+      })
+      .catch(reject)
+  })
+}
 
-const Module = module.constructor;
+const Module = module.constructor
 
-const mfs = new MemoryFs();
-const serverComplier = webpack(serverConfig);
-serverComplier.outputFileSystem = mfs;
-let serverBundle;
+const mfs = new MemoryFs()
+const serverComplier = webpack(serverConfig)
+serverComplier.outputFileSystem = mfs
+let serverBundle
 serverComplier.watch({}, (err, status) => {
-	if (err) throw err;
-	status = status.toJson();
-	status.errors.forEach(err => console.error(err));
-	status.warnings.forEach(warn => console.warn(warn));
+  if (err) throw err
+  status = status.toJson()
+  status.errors.forEach(err => console.error(err))
+  status.warnings.forEach(warn => console.warn(warn))
 
-	const bundlePath = path.join(
-		serverConfig.output.path,
-		serverConfig.output.filename
-	);
-	const bundle = mfs.readFileSync(bundlePath, 'utf-8');
-	const m = new Module();
-	m._compile(bundle, 'server-entry.js'); // 动态编译，指定文件名-> 无法在缓存中读取
-	serverBundle = m.exports.default; // exports==> network-localhost <div>为空
-});
+  const bundlePath = path.join(
+    serverConfig.output.path,
+    serverConfig.output.filename
+  )
+  const bundle = mfs.readFileSync(bundlePath, 'utf-8')
+  const m = new Module()
+  m._compile(bundle, 'server-entry.js') // 动态编译，指定文件名-> 无法在缓存中读取
+  serverBundle = m.exports.default // exports==> network-localhost <div>为空
+})
 
 module.exports = function (app) {
-
-	app.use('/public', proxy({
-		target: 'http://localhost:8888'
-	}));
-	app.get('*', function (req, res) {
-		getTemplate().then(template => {
-			const content = ReactDomServer.renderToString(serverBundle);
-			res.send(template.replace('<!--app-->', content))
-		})
-	})
-};
- 
+  app.use('/public', proxy({
+    target: 'http://localhost:8888'
+  }))
+  app.get('*', function (req, res) {
+    getTemplate().then(template => {
+      const content = ReactDomServer.renderToString(serverBundle)
+      res.send(template.replace('<!--app-->', content))
+    })
+  })
+}
