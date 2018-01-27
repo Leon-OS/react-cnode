13b8da8ae1f739817304453a69ae55e48a307537
zengfei <453141759@qq.com>
2018-01-27 14:21:15 +0800

    add Router

--- client/app.js
+++ client/app.js
@@ -4,6 +4,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader' // eslint-disable-line
+import { BrowserRouter } from 'react-router-dom'
 import App from './views/App'
 
 // ReactDOM.hydrate(<App/>, document.getElementById('root'));
@@ -12,7 +13,9 @@ const root = document.getElementById('root');
 const render = (Component) => {
   ReactDOM.hydrate(
     <AppContainer>
-      <Component />
+      <BrowserRouter>
+        <Component />
+      </BrowserRouter>
     </AppContainer>,
     root,
   )
@@ -21,8 +24,8 @@ const render = (Component) => {
 render(App);
 
 if (module.hot) {
-  module.hot.accept('./views/App.jsx', () => {
-    const NextApp = require('./views/App.jsx').default;
+  module.hot.accept('./views/App', () => {
+    const NextApp = require('./views/App').default;
     // ReactDOM.hydrate(<NextApp/>, document.getElementById("root"))
     render(NextApp)
   })
--- client/views/App.jsx
+++ client/views/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Routes from '../config/route'
 
-export default () => (
-  <div>hello babel 123</div>
-)
+export default class App extends React.Component {
+  componentDidMount() {
+    // dosomething
+  }
 
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <div>hello babel</div>
-//     )
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <div>
+          <Link to="/list">首页</Link>
+          <br />
+          <Link to="/detail">最终页</Link>
+        </div>
+        <Routes />
+      </div>
+    )
+  }
+}
